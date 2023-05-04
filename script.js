0482bbdc1b1da11daabd702a6695c8c7ccbfbc7f
Jean Adames <jeancadames22@gmail.com>
2023-05-03 22:10:48 -0400

    creation of multipages for website

--- script.js
+++ script.js
@@ -3,6 +3,31 @@ const navbarLinks = document.querySelectorAll("[data-nav-link]");
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 const navToggler = document.querySelector("[data-nav-toggler]");
+const closeBtns = document.querySelectorAll('#close');
+const fixedModalTerms = document.querySelector('.fixed-container#terms');
+const fixedModalConditions = document.querySelector('.fixed-container#privacy');
+
+const closeModal = function (){
+  closeBtns.forEach(closeBtn => {
+    closeBtn.addEventListener('click', ()=> {
+      fixedModalTerms.style.display = 'none';
+      fixedModalConditions.style.display = 'none';
+    })
+  });
+}
+
+
+closeModal();
+
+const showTerms = function(){
+  fixedModalTerms.style.display = 'block';
+  document.body.classList.add('overlay');
+}
+
+const showPrivacy = function(){
+  fixedModalConditions.style.display = 'block';
+  document.body.classList.add('overlay');
+}
 
 const addEventOnElem = function (elem, type, callback) {
   if (elem.length > 1) {
